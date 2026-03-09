c02c161fe87ec2bd955f4dae39bb349dea9f328d
jack62620000 <jack62620000@gmail.com>
2026-03-09 17:06:30 +0800

    Update overview.py

--- ui/overview.py
+++ ui/overview.py
@@ -171,7 +171,7 @@ def render_overview(d, code_input):
         showlegend=True
     ),
     row=1, col=1
-)
+        
 
     line_map = [
         ("ma5", "MA5"),
@@ -306,4 +306,5 @@ def render_overview(d, code_input):
                 use_container_width=True,
                 height=320
             )
+
 
